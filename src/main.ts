ebdecffc448411c82e1738b20931d33e3cf5fa65
EdwardMayk <samaelminina322@gmail.com>
2023-03-31 19:19:17 -0500

    CORS SOLUTION

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
+
   await app.listen(3002);
 }
 bootstrap();
