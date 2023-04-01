bee60dee746b4be39cc2324f6804f6578f3e2a47
EdwardMayk <samaelminina322@gmail.com>
2023-03-31 19:11:15 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
   await app.listen(3002);
 }
 bootstrap();
